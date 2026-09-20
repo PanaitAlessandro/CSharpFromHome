1ad6492a38360be4b39324a88a06bd37a27726cb
PanaitAlessandro <14529922@itisrossi.vi.it>
2026-09-20 22:05:38 +0200

    feat(lezione15/es1): campi e proprietà CAutoElettrica

--- Lezioni/Lezione15/Esercizio1/CAutoElettrica.cs
+++ Lezioni/Lezione15/Esercizio1/CAutoElettrica.cs
@@ -2,5 +2,38 @@ using System;
 
 namespace Esercizio1
 {
-    
+    public class CAutoElettrica : CAuto
+    {
+        
+        // per l'auto elettrico direi di mettere autonomia e modello (tipo Tesla model y)
+
+        private int _autonomia;
+        private string _modello;
+
+        public int Autonomia
+        {
+            get {return _autonomia;}
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("L'autonomia non può essere zero o minore");
+                }
+                _autonomia = value;
+            }
+        }
+
+        public string Modello
+        {
+            get {return _modello;}
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("è la sesta volta che faccio questo controllo. IL MODELLO NON PUO ESSERE NULL");
+                }
+                _modello = value;
+            }
+        }
+    }
 }
