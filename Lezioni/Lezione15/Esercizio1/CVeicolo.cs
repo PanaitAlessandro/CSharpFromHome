ce656d5a7ea747ca2cb6f720e49a82387f9a4342
PanaitAlessandro <14529922@itisrossi.vi.it>
2026-09-20 21:43:17 +0200

    feat(lezione15/es1): campi e properties di CVeicolo

--- Lezioni/Lezione15/Esercizio1/CVeicolo.cs
+++ Lezioni/Lezione15/Esercizio1/CVeicolo.cs
@@ -2,5 +2,55 @@ using System;
 
 namespace Esercizio1
 {
-    
+    public class CVeicolo
+    {
+        // facciamo targa, cilindrata, chilometraggio
+
+        private string _targa;
+        private int _cilindrata;
+        private int _chilometraggio;
+
+        public string Targa
+        {
+            get { return _targa; }
+            set
+            {
+                if(string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("la targa non può essere null");
+                }
+
+                _targa = value;
+            }
+        }
+        public int Cilindrata
+        {
+            get { return _cilindrata; }
+            set
+            {
+                if(value <= 0)
+                {
+                    throw new ArgumentException("la cilindrata non può essere minore o uguale a zero");
+                }
+
+                _cilindrata = value;
+            }
+        }
+
+        public int Chilometraggio
+        {
+            get { return _chilometraggio; }
+            set
+            {
+                if(value < 0)
+                {
+                    throw new ArgumentException("Il chilometraggio non può scendere sotto lo zero (altro che KM zero)");
+                }
+
+                _chilometraggio = value;
+            }
+        }
+
+        
+    }
 }
