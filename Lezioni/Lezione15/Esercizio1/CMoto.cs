081a384f05611e221c45513710b4c446c108823c
PanaitAlessandro <14529922@itisrossi.vi.it>
2026-09-20 21:59:04 +0200

    feat(lezione15/es1): campo e proprietà di CMoto

--- Lezioni/Lezione15/Esercizio1/CMoto.cs
+++ Lezioni/Lezione15/Esercizio1/CMoto.cs
@@ -2,5 +2,25 @@ using System;
 
 namespace Esercizio1
 {
-    
+    public class CMoto : CVeicolo
+    {
+        // anche qui voglio passare il marchio (Yamaha per esempio)
+
+        private string _marca;
+
+        public string Marca
+        {
+            get { return _marca; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("La marca non può essere null");
+                }
+                _marca = value;
+            }
+        }
+
+        
+    }
 }
