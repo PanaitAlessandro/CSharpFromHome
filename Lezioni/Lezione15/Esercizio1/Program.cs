d92fe0219dbf2033ddff702655ebfc6edb594cea
PanaitAlessandro <14529922@itisrossi.vi.it>
2026-09-20 22:14:18 +0200

    feat(lezione15/es1): main program

--- Lezioni/Lezione15/Esercizio1/Program.cs
+++ Lezioni/Lezione15/Esercizio1/Program.cs
@@ -9,6 +9,9 @@ namespace Esercizio1
     {
         static void Main(string[] args)
         {
+            CAutoElettrica tesla = new CAutoElettrica(100,"Tesla model y", "Tesla","ABCED",200,4000); // devo mettere: int autonomia, string modello, string marca, string targa, int cilindrata, int chilometraggio
+
+            System.Console.WriteLine(tesla);
         }
     }
 }
