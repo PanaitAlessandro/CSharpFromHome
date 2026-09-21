5833323f5ce1cc6623615607fd1aadc8059b3805
PanaitAlessandro <14529922@itisrossi.vi.it>
2026-09-21 17:35:53 +0200

    feat(lezione16): due commenti rapidi per rappresentare il poliformismo

--- Lezioni/Lezione16/Lezione16.cs
+++ Lezioni/Lezione16/Lezione16.cs
@@ -4,4 +4,7 @@ namespace Lezione16
 {
     // CVeicolo v = new CMoto(); -> poliformismo, ossia: creo un veicolo che è una MOTO.
     // il poliformismo si fa con IS-A
+
+    // ora facciamo finta di chiamare un metodo v.Rumore(), deve far partire quello di CVeicolo o di CMoto?
+    // per questo virtual e override servono, per definire questi casi
 }
