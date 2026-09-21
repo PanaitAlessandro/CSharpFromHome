314a5ec7004aca33e8e23d303dd19178e87672ca
PanaitAlessandro <14529922@itisrossi.vi.it>
2026-09-21 17:41:22 +0200

    feat(lezione16): primi passi su virtual e override

--- Lezioni/Lezione16/Lezione16.cs
+++ Lezioni/Lezione16/Lezione16.cs
@@ -7,4 +7,28 @@ namespace Lezione16
 
     // ora facciamo finta di chiamare un metodo v.Rumore(), deve far partire quello di CVeicolo o di CMoto?
     // per questo virtual e override servono, per definire questi casi
+
+public class CVeicolo2
+{
+    public virtual string Rumore()
+    {
+        return "...";
+    }
+}
+
+public class CMoto2 : CVeicolo2
+{
+    public override string Rumore()
+    {
+        return "Vroom";
+    }
+}
+
+public class CAuto2 : CVeicolo2
+{
+    public override string Rumore()
+    {
+        return "Brum";
+    }
+}
 }
