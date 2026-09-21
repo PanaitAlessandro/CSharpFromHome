5184a939dc2f64e80b6c1a6caf2bda753e7905a9
PanaitAlessandro <14529922@itisrossi.vi.it>
2026-09-21 18:07:48 +0200

    feat(lezione16): rappresentazione del main program con un foreach

--- Lezioni/Lezione16/Lezione16-2.cs
+++ Lezioni/Lezione16/Lezione16-2.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Net.Mail;
 public class CVeicolo
 {
     public virtual string Rumore()
@@ -28,5 +29,23 @@ public class CAutoElettrica : CAuto
     public override string Rumore()
     {
         return "Ssss";
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        CVeicolo[] v = 
+        {
+            new CMoto(),
+            new CAuto(),
+            new CAutoElettrica()
+        };
+
+        foreach (CVeicolo veicolo in v)
+        {
+            System.Console.WriteLine(veicolo.Rumore());
+        }
     }
 }
