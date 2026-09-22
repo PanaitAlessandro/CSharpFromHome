4d32754401615a82191d9f064d33b9cbfd75b032
PanaitAlessandro <14529922@itisrossi.vi.it>
2026-09-22 16:09:39 +0200

    docs(lezione17): mini commenti per rendere il codice pulito

--- Lezioni/Lezione17/Lezione17.cs
+++ Lezioni/Lezione17/Lezione17.cs
@@ -24,6 +24,6 @@ namespace  Lezione17
         }
     }
 
-    public abstract int CalcolaCostoTagliando();
+    public abstract int CalcolaCostoTagliando(); // un metodo può essere astratto ma non avrà nessun contenuto {}
 }
 }
