936003388b11613f456dff4e7a67a18b842f0d4a
PanaitAlessandro <14529922@itisrossi.vi.it>
2026-09-22 15:42:37 +0200

    feat(lezione17): riportato lo statement

---
+++ Lezioni/Lezione17/Lezione17.cs
@@ -0,0 +1,7 @@
+using System;
+
+namespace  Lezione17 
+{
+    // Ieri CVeicolo.CalcolaCostoTagliando() ritornava 100. Numero inventato.. un veicolo generico non esiste, quindi quel valore non significa niente.
+    // Peggio: niente vietava new CVeicolo(...) con quel costo finto.
+}
