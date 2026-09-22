6a990e9e7afa2fd15a36d830fb99d2e147835931
PanaitAlessandro <14529922@itisrossi.vi.it>
2026-09-22 15:43:14 +0200

    feat(lezione17): definita la soluzione

--- Lezioni/Lezione17/Lezione17.cs
+++ Lezioni/Lezione17/Lezione17.cs
@@ -4,4 +4,7 @@ namespace  Lezione17
 {
     // Ieri CVeicolo.CalcolaCostoTagliando() ritornava 100. Numero inventato.. un veicolo generico non esiste, quindi quel valore non significa niente.
     // Peggio: niente vietava new CVeicolo(...) con quel costo finto.
+
+    // soluzione:
+    // dire al compilatore "questo metodo DEVE essere riscritto da ogni figlia, e la base non si può creare da sola"
 }
